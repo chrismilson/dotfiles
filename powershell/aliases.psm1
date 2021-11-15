5e4c8a9cbea8dc286a197de26b24065d9a4ccdb2
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2021-11-15 14:41:03 +1300

    Add clipoard aliases

--- powershell/aliases.psm1
+++ powershell/aliases.psm1
@@ -3,3 +3,7 @@ Set-Alias -Name sa -Value Set-Alias
 
 # Git shortening makes the world happier
 Set-Alias -Name g -Value git
+
+# Clipboard aliases
+Set-Alias -Name pbcopy -Value Set-Clipboard
+Set-Alias -Name pbpaste -Value Get-Clipboard
