d005456c6d8be4edbc2777d3836d361649093f00
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2022-01-19 11:46:30 +1300

    Rename target if specified

--- powershell/functions/gitclone.psm1
+++ powershell/functions/gitclone.psm1
@@ -1,11 +1,16 @@
 # Clones a repository and then goes into said repository
 function gitclone {
     param($1, $2)
+    
+    if (-Not $1) {
+        $1 = Get-Clipboard
+    }
 
     if (-Not $2) {
         $2 = [io.path]::GetFileNameWithoutExtension($1)
     }
     
-    git clone $1
+    git clone $1 $2
     Set-Location $2
 }
+
