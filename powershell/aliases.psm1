db148abf48085d5eb4370d5a818c9e94472656f4
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2022-01-20 14:04:12 +1300

    Add aliases

--- powershell/aliases.psm1
+++ powershell/aliases.psm1
@@ -7,3 +7,20 @@ Set-Alias -Name g -Value git
 # Clipboard aliases
 Set-Alias -Name pbcopy -Value Set-Clipboard
 Set-Alias -Name pbpaste -Value Get-Clipboard
+
+# Missing Bash Alias
+Set-Alias -Name time -Value Measure-Command
+
+# Use ls.exe if available
+if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:ls -ErrorAction SilentlyContinue
+
+    ${function:ls} = { ls.exe --color @args }
+    ${function:ll} = { ls -lF @args }
+    ${function:la} = { ls -laF @args }
+} else {
+    ${function:ls} = { ls -Force @args }
+}
+
+# Determin size of a file or total size of a directory
+Set-Alias -Name fs -Value Get-DiskUsage
