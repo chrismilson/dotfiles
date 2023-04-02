aca561d2e4ad11785d05b5bf48646956958d7a04
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2023-04-02 23:38:41 +0000

    Search powershell history based on typed text

--- powershell/options.psm1
+++ powershell/options.psm1
@@ -1,2 +1,6 @@
 # Make tab completion more like bash
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+# Search history based on the currently typed text
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
